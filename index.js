2e1ffcbdec1e3b797bf1f8915ed76b8f77366031
internet <tikhonov.a.d@sberbank.ru>
2020-02-14 14:06:08 +0300

    Init positionning

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+//width: 460px
+//height: 470px
 const show = document.createElement('pre');
+show.style.margin = 0;
+const main = document.createElement('div');
+document.body.style.display = 'flex';
+main.style.margin = 'auto';
+main.appendChild(show);
+document.body.appendChild(main);
 
-const interval = setInterval(() => {
-    THE_WORLD.tick();
-    show.innerText = THE_WORLD.print();
-},100);
+let interval = null;
+const startGame = () => {
+    interval = setInterval(() => {
+        THE_WORLD.tick();
+        show.innerText = THE_WORLD.print();
+    },100);
+}
 
-document.body.appendChild(show);
+const stopGame = () => {
+    THE_WORLD.startTimer(); 
+    clearInterval(interval);
+}
+
+startGame();
+
+
+
