3880b640c8ce2297fc29ebd7e2b72c40b9def0a0
internet <tikhonov.a.d@sberbank.ru>
2020-02-18 09:11:32 +0300

    TASK-27 Falling food kills

--- world.js
+++ world.js
@@ -277,11 +277,16 @@ class Star {
         this.x = x;
         this.y = y;
         this.still_here = true;
+        this.falling = false;
+        this.killer = false;
     }
 
     static scores = 0;
 
     check_way_down(world) {
+        if (this.falling && world[this.y+1][this.x] === PLAYER) { this.killer = true; Player.off = true; }
+        else if (this.falling && '/-|\\'.includes(world[this.y+1][this.x])) { this.killer = true; }
+        this.falling = true;
         return  world[this.y+1] && world[this.y+1][this.x] === EMPTY;
     }
 
@@ -290,10 +295,12 @@ class Star {
     }
 
     check_way_left(world) {
+        this.falling = true;
         return world[this.y][this.x-1] === EMPTY && world[this.y+1][this.x-1] === EMPTY;
     }
 
     check_way_right(world) {
+        this.falling = true;
         return world[this.y][this.x+1] === EMPTY && world[this.y+1][this.x+1] === EMPTY;
     }
 
@@ -302,8 +309,10 @@ class Star {
         else if (this.check_way_down(world)) this.y += 1;
         else if (this.move_possible(world)) {
             if (this.check_way_left(world)) this.x -= 1;
-             else if (this.check_way_right(world)) this.x += 1;
+            else if (this.check_way_right(world)) this.x += 1;
+            else this.falling = false;
         }
+        else this.falling = false;
 
     }
 }
