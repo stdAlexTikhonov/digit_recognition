dec1d5ec7c71ad94813f4fa71cf9aa4c5a3b9b45
stdAlexTikhonov <std1992@mail.ru>
2020-05-16 09:52:59 +0300

    TASK-30 Favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -23,7 +24,16 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: "./index.html"
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src/favicon.ico"),
+                    to: path.resolve(__dirname, 'dist'),
+                    force: true
+                }
+            ]
+        })
     ],
     module: {
         rules: [
